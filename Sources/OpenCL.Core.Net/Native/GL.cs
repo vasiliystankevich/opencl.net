4ef9e2b32c7c971f7cfb1a3cdbbbfa51357ea168
vasiliy.stan <vasiliyStankevich@gmail.com>
2020-02-18 12:37:48 +0200

    OpenCL.Core.Net - refactoring

--- Sources/OpenCL.Core.Net/Native/GL.cs
+++ Sources/OpenCL.Core.Net/Native/GL.cs
@@ -1,58 +1,69 @@
-using OpenCL.Core.Net.Native;
+using System;
+using System.Runtime.InteropServices;
 using OpenCL.Types.Core.Net.Enums;
 using OpenCL.Types.Core.Net.Enums.Mem;
 using OpenCL.Types.Core.Net.Primitives;
-using System;
-using System.Runtime.InteropServices;
 
-namespace OpenCL.Core.Net
+namespace OpenCL.Core.Net.Native
 {
-    public class GLApi
+    public class GlApi
     {
         [DllImport(DllNative.Name)]
-        public static extern Mem clCreateFromGLBuffer(Context context, MemFlags flags, uint bufobj, ref Error errcode_ret);
+        public static extern Mem clCreateFromGLBuffer(Context context, MemFlags flags, uint bufobj,
+            ref Error errcodeRet);
 
         [DllImport(DllNative.Name)]
-        public static extern Mem clCreateFromGLBuffer(Context context, MemFlags flags, uint bufobj, IntPtr errcode_ret);
+        public static extern Mem clCreateFromGLBuffer(Context context, MemFlags flags, uint bufobj, IntPtr errcodeRet);
 
         [DllImport(DllNative.Name)]
-        public static extern Mem clCreateFromGLTexture(Context context, MemFlags flags, int target, int miplevel, uint texture, ref Error errcode_ret);
+        public static extern Mem clCreateFromGLTexture(Context context, MemFlags flags, int target, int miplevel,
+            uint texture, ref Error errcodeRet);
 
         [DllImport(DllNative.Name)]
-        public static extern Mem clCreateFromGLTexture(Context context, MemFlags flags, int target, int miplevel, uint texture, IntPtr errcode_ret);
-
-        [Obsolete("Deprecated since OpenCL 1.2")]
-        [DllImport(DllNative.Name)]
-        public static extern Mem clCreateFromGLTexture2D(Context context, MemFlags flags, int target, int miplevel, uint texture, ref Error errcode_ret);
-
-        [Obsolete("Deprecated since OpenCL 1.2")]
-        [DllImport(DllNative.Name)]
-        public static extern Mem clCreateFromGLTexture2D(Context context, MemFlags flags, int target, int miplevel, uint texture, IntPtr errcode_ret);
+        public static extern Mem clCreateFromGLTexture(Context context, MemFlags flags, int target, int miplevel,
+            uint texture, IntPtr errcodeRet);
 
         [Obsolete("Deprecated since OpenCL 1.2")]
         [DllImport(DllNative.Name)]
-        public static extern Mem clCreateFromGLTexture3D(Context context, MemFlags flags, int target, int miplevel, uint texture, ref Error errcode_ret);
+        public static extern Mem clCreateFromGLTexture2D(Context context, MemFlags flags, int target, int miplevel,
+            uint texture, ref Error errcodeRet);
 
         [Obsolete("Deprecated since OpenCL 1.2")]
         [DllImport(DllNative.Name)]
-        public static extern Mem clCreateFromGLTexture3D(Context context, MemFlags flags, int target, int miplevel, uint texture, IntPtr errcode_ret);
+        public static extern Mem clCreateFromGLTexture2D(Context context, MemFlags flags, int target, int miplevel,
+            uint texture, IntPtr errcodeRet);
+
+        [Obsolete("Deprecated since OpenCL 1.2")]
+        [DllImport(DllNative.Name)]
+        public static extern Mem clCreateFromGLTexture3D(Context context, MemFlags flags, int target, int miplevel,
+            uint texture, ref Error errcodeRet);
+
+        [Obsolete("Deprecated since OpenCL 1.2")]
+        [DllImport(DllNative.Name)]
+        public static extern Mem clCreateFromGLTexture3D(Context context, MemFlags flags, int target, int miplevel,
+            uint texture, IntPtr errcodeRet);
 
         [DllImport(DllNative.Name)]
-        public static extern Mem clCreateFromGLRenderbuffer(Context context, MemFlags flags, uint renderbuffer, ref Error errcode_ret);
+        public static extern Mem clCreateFromGLRenderbuffer(Context context, MemFlags flags, uint renderbuffer,
+            ref Error errcodeRet);
 
         [DllImport(DllNative.Name)]
-        public static extern Mem clCreateFromGLRenderbuffer(Context context, MemFlags flags, uint renderbuffer, IntPtr errcode_ret);
+        public static extern Mem clCreateFromGLRenderbuffer(Context context, MemFlags flags, uint renderbuffer,
+            IntPtr errcodeRet);
 
         [DllImport(DllNative.Name)]
-        public static extern Error clGetGLObjectInfo(Mem memobj, ref uint gl_object_type, ref uint gl_object_name);
+        public static extern Error clGetGLObjectInfo(Mem memobj, ref uint glObjectType, ref uint glObjectName);
 
         [DllImport(DllNative.Name)]
-        public static extern Error clGetGLTextureInfo(Mem memobj, uint param_name, SizeT param_value_size, IntPtr param_value, ref SizeT param_value_size_ret);
+        public static extern Error clGetGLTextureInfo(Mem memobj, uint paramName, SizeT paramValueSize,
+            IntPtr paramValue, ref SizeT paramValueSizeRet);
 
         [DllImport(DllNative.Name)]
-        public static extern Error clEnqueueAcquireGLObjects(CommandQueue command_queue, uint num_objects, [In] Mem[] mem_objects, uint num_events_in_wait_list, [In] Event[] event_wait_list, ref Event e);
+        public static extern Error clEnqueueAcquireGLObjects(CommandQueue commandQueue, uint numObjects,
+            [In] Mem[] memObjects, uint numEventsInWaitList, [In] Event[] eventWaitList, ref Event e);
 
         [DllImport(DllNative.Name)]
-        public static extern Error clEnqueueReleaseGLObjects(CommandQueue command_queue, uint num_objects, [In] Mem[] mem_objects, uint num_events_in_wait_list, [In] Event[] event_wait_list, ref Event e);
+        public static extern Error clEnqueueReleaseGLObjects(CommandQueue commandQueue, uint numObjects,
+            [In] Mem[] memObjects, uint numEventsInWaitList, [In] Event[] eventWaitList, ref Event e);
     }
 }
