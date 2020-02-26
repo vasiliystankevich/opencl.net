12af70c3896b09b3f8bafc977237172bac900989
vasiliy.stan <vasiliyStankevich@gmail.com>
2020-02-26 17:24:28 +0200

    OpenCL.Core.Net - refactoring

--- Sources/OpenCL.Core.Net.Tests/Api/ErrorValidator.cs
+++ Sources/OpenCL.Core.Net.Tests/Api/ErrorValidator.cs
@@ -1,7 +1,9 @@
-﻿using Moq;
+﻿using AutoFixture.Xunit2;
+using Moq;
 using OpenCL.Core.Net.Api;
 using OpenCL.Core.Net.Types;
 using OpenCL.Core.Net.Types.Enums;
+using OpenCL.Core.Net.Types.Primitives;
 using Tests.AAAPattern.xUnit.Attributes;
 using Xunit;
 
@@ -11,7 +13,7 @@ namespace OpenCL.Core.Net.Tests.Api
     {
         [Theory]
         [MoqAutoData]
-        public void ValidateExecuteTest(Mock<NativeFunc> functor, ErrorValidator sut)
+        public void NativeFuncValidateExecuteTest(Mock<NativeFunc> functor, ErrorValidator sut)
         {
             //arrange
             functor.Setup(service => service()).Returns(Error.Success);
@@ -25,7 +27,7 @@ namespace OpenCL.Core.Net.Tests.Api
 
         [Theory]
         [MoqInlineAutoData(Error.InvalidOperation)]
-        public void ValidateExceptionTest(Error actual, Mock<NativeFunc> functor, ErrorValidator sut)
+        public void NativeFuncValidateExceptionTest(Error actual, Mock<NativeFunc> functor, ErrorValidator sut)
         {
             //arrange
             functor.Setup(service => service()).Returns(actual);
@@ -37,6 +39,50 @@ namespace OpenCL.Core.Net.Tests.Api
             functor.Verify(service => service(), Times.AtLeastOnce);
             Assert.Equal(expected.ErrorCode, actual);
         }
+
+        [Theory]
+        [MoqAutoData]
+        public void NativeFunc1RefValidateExecuteTest(SizeT data, Mock<NativeFunc1Ref<SizeT, Error>> functor, ErrorValidator sut)
+        {
+            //arrange
+            functor.Setup(service => service(ref It.Ref<SizeT>.IsAny)).Returns(Error.Success);
+
+            //act
+            sut.Validate(ref data, functor.Object);
+
+            //assert
+            functor.Verify(service => service.Invoke(ref It.Ref<SizeT>.IsAny), Times.AtLeastOnce);
+        }
+
+        [Theory]
+        [MoqInlineAutoData(Error.InvalidOperation)]
+        public void NativeFunc1RefValidateExceptionTest(Error actual, SizeT data, [Frozen]Mock<NativeFunc1Ref<SizeT, Error>> functor, ErrorValidator sut)
+        {
+            //arrange
+            functor.Setup(service => service(ref It.Ref<SizeT>.IsAny)).Returns(actual);
+
+            //act
+            var expected = Assert.Throws<Exception>(() => sut.Validate(ref data, functor.Object));
+
+            //assert
+            functor.Verify(service => service.Invoke(ref It.Ref<SizeT>.IsAny), Times.AtLeastOnce);
+            Assert.Equal(expected.ErrorCode, actual);
+        }
+
+        //[Theory]
+        //[MoqInlineAutoData(Error.InvalidOperation)]
+        //public void ValidateExceptionTest(Error actual, Mock<NativeFunc> functor, ErrorValidator sut)
+        //{
+        //    //arrange
+        //    functor.Setup(service => service()).Returns(actual);
+
+        //    //act
+        //    var expected = Assert.Throws<Exception>(() => sut.Validate(functor.Object));
+
+        //    //assert
+        //    functor.Verify(service => service(), Times.AtLeastOnce);
+        //    Assert.Equal(expected.ErrorCode, actual);
+        //}
 
         //public void Validate(ref SizeT size, NativeFunc1Ref<SizeT, Error> functor)
         //{
