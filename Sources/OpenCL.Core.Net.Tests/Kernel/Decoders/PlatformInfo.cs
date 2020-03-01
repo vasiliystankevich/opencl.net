8427d7d2671341546841dfac3d52865caf61b3d2
vasiliy.stan <vasiliyStankevich@gmail.com>
2020-03-01 19:51:33 +0200

    OpenCL.Core.Net - refactoring

--- Sources/OpenCL.Core.Net.Tests/Kernel/Decoders/PlatformInfo.cs
+++ Sources/OpenCL.Core.Net.Tests/Kernel/Decoders/PlatformInfo.cs
@@ -1,8 +1,10 @@
-﻿using AutoFixture.Xunit2;
+﻿using System;
+using AutoFixture.Xunit2;
 using Moq;
 using OpenCL.Core.Net.Interfaces.Kernel;
 using OpenCL.Core.Net.Interfaces.Kernel.Executors;
 using OpenCL.Core.Net.Kernel.Decoders;
+using OpenCL.Core.Net.Types.Enums;
 using OpenCL.Core.Net.Types.Primitives;
 using Tests.AAAPattern.xUnit.Attributes;
 using Xunit;
@@ -12,16 +14,28 @@ namespace OpenCL.Core.Net.Tests.Kernel.Decoders
     public class PlatformInfoDecoderTests
     {
         [Theory]
-        [MoqAutoData]
-        public void DecodeTest(PlatformId z, SizeT data,  [Frozen]Mock<IPlatformKernel> kernel, [Frozen]Mock<IMarshalExecutor> marshal, PlatformInfoDecoder sut)
+        [MoqInlineAutoData(PlatformInfo.Profile)]
+        [MoqInlineAutoData(PlatformInfo.Version)]
+        [MoqInlineAutoData(PlatformInfo.Name)]
+        [MoqInlineAutoData(PlatformInfo.Vendor)]
+        [MoqInlineAutoData(PlatformInfo.Extensions)]
+        public void DecodeTest(PlatformInfo info, PlatformId platform, SizeT paramValueSize, SizeT resultSize, int ptrValue, string actual, [Frozen]Mock<IPlatformKernel> kernel, [Frozen]Mock<IMarshalExecutor> marshal, PlatformInfoDecoder sut)
         {
             //arrange
-            //var platformId = (PlatformId) platformIdValue;
-            
+            var ptr = new IntPtr(ptrValue);
+            marshal.Setup(service => service.AllocHGlobal(paramValueSize)).Returns(ptr);
+            kernel.Setup(service => service.GetPlatformInfo(platform, info, paramValueSize, ptr)).Returns(resultSize);
+            marshal.Setup(service => service.PtrToStringAnsi(ptr, resultSize)).Returns(actual);
+
             //act
-            //var expected = sut.Decode(platformId, )
+            var expected = sut.Decode(platform, info, paramValueSize);
 
             //assert
+            marshal.Verify(service => service.AllocHGlobal(paramValueSize), Times.AtLeastOnce);
+            kernel.Verify(service => service.GetPlatformInfo(platform, info, paramValueSize, ptr), Times.AtLeastOnce);
+            marshal.Verify(service => service.PtrToStringAnsi(ptr, resultSize), Times.AtLeastOnce);
+            marshal.Verify(service=>service.FreeHGlobal(ptr), Times.AtLeastOnce);
+            Assert.Equal(expected, actual);
         }
     }
 }
