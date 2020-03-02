ec93496f8c043e2b8808866f70aad820b802d22e
vasiliy.stan <vasiliyStankevich@gmail.com>
2020-03-02 16:01:06 +0200

    OpenCL.Core.Net - refactoring

--- Sources/OpenCL.Core.Net.Types/Func.cs
+++
@@ -1,9 +0,0 @@
-﻿using OpenCL.Core.Net.Types.Enums;
-
-namespace OpenCL.Core.Net.Types
-{
-    public delegate Error NativeFunc();
-    public delegate TResult NativeFunc1Ref<TArg, out TResult>(ref TArg arg);
-    public delegate TResult NativeFunc2Ref<TArg1, TArg2, out TResult>(ref TArg1 arg1, ref TArg2 arg2);
-    public delegate TResult NativeFunc1Arg<in TArg, out TResult>(TArg arg);
-}
